cf973a97a7a415d93c8e9031db844b67b39eb4c8
Dan Croak <dan@statusok.com>
2018-03-15 20:42:15 +0000

    sdk/node: fix erratic feeds test
    This test was behaving erratically;
    sometimes it passed and sometimes it failed.
    
    ```
    expect(actionFeedItems).to.deep.equal([tx1.actions[0].id,
    tx2.actions[0].id])
    ```
    
    The `feed.consume` method is not returning a `then()`-able
    `Promise` so `await` was a no-op and we had a race condition
    between the `expect` and the second transaction getting
    processed by the feed.
    
    This also applies our `$CHAIN/prettier.config.js` formatting
    to the file.
    
    Closes #2819
    
    Author: Dan Croak <dan@statusok.com>
    Date: Thu Mar 15 13:23:40 2018 -0700
    upstream:f0e79e8db0cd55b1094ad0d2f720ae037cb05588

--- test/feeds.ts
+++ test/feeds.ts
@@ -11,84 +11,66 @@ const expect = chai.expect
 
 import { testHelpers } from './testHelpers'
 
-const {
-  client,
-  createAccount,
-  createFlavor,
-} = testHelpers
+const { client, createAccount, createFlavor } = testHelpers
 
 describe('Feed', () => {
   it('creates action feed', async () => {
-    const feed = await client.feeds.create(
-      {
-        type: 'action',
-        id: `actionFeed-${uuid.v4()}`,
-        filter: "tags.type=$1",
-        filterParams: ["test"],
-      }
-    )
+    const feed = await client.feeds.create({
+      type: 'action',
+      id: `actionFeed-${uuid.v4()}`,
+      filter: 'tags.type=$1',
+      filterParams: ['test'],
+    })
     expect(feed.type).to.equal('action')
   })
 
   it('creates transaction feed', async () => {
-    const feed = await client.feeds.create(
-      {
-        type: 'transaction',
-        id: `transactionFeed-${uuid.v4()}`,
-        filter: "actions(type='issue')",
-      }
-    )
+    const feed = await client.feeds.create({
+      type: 'transaction',
+      id: `transactionFeed-${uuid.v4()}`,
+      filter: "actions(type='issue')",
+    })
     expect(feed.type).to.equal('transaction')
   })
 
   it('retrieves feed by id', async () => {
     const feedId = `actionFeed-${uuid.v4()}`
-    await client.feeds.create(
-      {
-        type: 'action',
-        id: feedId,
-        filter: "tags.type=$1",
-        filterParams: ["test"],
-      }
-    )
+    await client.feeds.create({
+      type: 'action',
+      id: feedId,
+      filter: 'tags.type=$1',
+      filterParams: ['test'],
+    })
 
-    const feed = await client.feeds.get({id: feedId})
+    const feed = await client.feeds.get({ id: feedId })
     expect(feed.type).to.equal('action')
     expect(feed.id).to.equal(feedId)
   })
 
   it('deletes feed by id', async () => {
     const feedId = `actionFeed-${uuid.v4()}`
-    await client.feeds.create(
-      {
-        type: 'action',
-        id: feedId,
-        filter: "tags.type=$1",
-        filterParams: ["test"],
-      }
-    )
+    await client.feeds.create({
+      type: 'action',
+      id: feedId,
+      filter: 'tags.type=$1',
+      filterParams: ['test'],
+    })
 
-    const resp = await client.feeds.delete({id: feedId})
-    expect(resp.message).to.equal("ok")
-    expect(client.feeds.get({ id: feedId } as any)).to.be.rejectedWith(
-      'SEQ002'
-    )
+    const resp = await client.feeds.delete({ id: feedId })
+    expect(resp.message).to.equal('ok')
+    expect(client.feeds.get({ id: feedId } as any)).to.be.rejectedWith('SEQ002')
   })
 
   it('lists feeds', async () => {
-    const actionFeed = await client.feeds.create(
-      {
-        type: 'action',
-        id: `actionFeed-${uuid.v4()}`,
-      }
-    )
+    const actionFeed = await client.feeds.create({
+      type: 'action',
+      id: `actionFeed-${uuid.v4()}`,
+    })
 
-    const transactionFeed = await client.feeds.create(
-      {
-        type: 'transaction',
-        id: `transactionFeed-${uuid.v4()}`,
-      }
-    )
+    const transactionFeed = await client.feeds.create({
+      type: 'transaction',
+      id: `transactionFeed-${uuid.v4()}`,
+    })
 
     const items: any[] = []
     await client.feeds.list().all(item => items.push(item.id))
@@ -99,33 +81,27 @@ describe('Feed', () => {
     const goldId: any = (await createFlavor('gold')).id
     const bobId: any = (await createAccount('bob')).id
     const tag: string = uuid.v4()
-
-    const actionFeed = await client.feeds.create(
-      {
-        type: 'action',
-        id: `actionFeed-${uuid.v4()}`,
-        filter: 'tags.type=$1',
-        filterParams: [tag],
-      }
-    )
-
-    const transactionFeed = await client.feeds.create(
-      {
-        type: 'transaction',
-        id: `transactionFeed-${uuid.v4()}`,
-        filter: 'actions(tags.type=$1)',
-        filterParams: [tag],
-      }
-    )
+    const actionFeed = await client.feeds.create({
+      type: 'action',
+      id: `actionFeed-${uuid.v4()}`,
+      filter: 'tags.type=$1',
+      filterParams: [tag],
+    })
+    const transactionFeed = await client.feeds.create({
+      type: 'transaction',
+      id: `transactionFeed-${uuid.v4()}`,
+      filter: 'actions(tags.type=$1)',
+      filterParams: [tag],
+    })
 
     const actionFeedItems: any[] = []
-    const actionResp = actionFeed.consume((action: any, next: any, stop: any) => {
+    actionFeed.consume((action: any, next: any, stop: any) => {
       actionFeedItems.push(action.id)
       actionFeedItems.length === 2 ? stop(true) : next(true)
     })
 
     const transactionFeedItems: any[] = []
-    const transactionResp = transactionFeed.consume((transaction: any, next: any, stop: any) => {
+    transactionFeed.consume((transaction: any, next: any, stop: any) => {
       transactionFeedItems.push(transaction.id)
       transactionFeedItems.length === 2 ? stop(true) : next(true)
     })
@@ -138,7 +114,6 @@ describe('Feed', () => {
         actionTags: { type: tag },
       })
     })
-
     const tx2 = await client.transactions.transact(builder => {
       builder.issue({
         flavorId: goldId,
@@ -148,9 +123,19 @@ describe('Feed', () => {
       })
     })
 
-    await actionResp
-    await transactionResp
-    expect(actionFeedItems).to.deep.equal([tx1.actions[0].id, tx2.actions[0].id])
+    // TODO we can't await a Promise returned by consume,
+    // but would like to
+    while (actionFeedItems.length <= 2) {
+      return true
+    }
+    while (transactionFeedItems.length <= 2) {
+      return true
+    }
+
+    expect(actionFeedItems).to.deep.equal([
+      tx1.actions[0].id,
+      tx2.actions[0].id,
+    ])
     expect(transactionFeedItems).to.deep.equal([tx1.id, tx2.id])
   })
 })
